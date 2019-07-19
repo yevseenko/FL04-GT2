017d1022bee8e8c7684a4954be0074b60e5241d6
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2019-07-19 19:42:31 +0300

    qweqwe

--- js/custom.js
+++ js/custom.js
@@ -12,12 +12,26 @@
     circle = document.querySelector('#circle');
 
   //ellipse
+  const verticalR = document.querySelector('#ellipseVR'),
+    horizontalR = document.querySelector('#ellipseHR'),
+    ellipse = document.querySelector('#ellipse');
+
   //square
+  const square = document.querySelector('#square'),
+    squareSide = document.querySelector('#squareSide');
+
   //rectangle
+  const rectangle = document.querySelector('#rectangle'),
+    rectangleSideA = document.querySelector('#rectangleSideA'),
+    rectangleSideB = document.querySelector('#rectangleSideB');
+
   //triangle
-  
+  const triangle = document.querySelector('#triangle'),
+    triangleSideA = document.querySelector('#triangleSideA'),
+    triangleSideB = document.querySelector('#triangleSideB');
+
   function changeParams(w, h, el) {
-    if (w > 30 || h > 30 || w < 0 || h < 0) {
+    if (w > 40 || h > 40 || w < 0 || h < 0) {
       alert(`Invalid value for element => ${el.getAttribute('name')}`);
       return;
     }
@@ -31,9 +45,19 @@
 
   btn.onclick = function () {
     changeParams(parSideA.value || 20, parSideB.value || 10, par);
-    changeSkew(parAngle.value || 25, par);
+    changeSkew(parAngle.value || 20, par);
 
     const containerHW = circleR.value * 2 || 10;
-    changeParams(containerHW, containerHW, circle);
+    changeParams(containerHW || 20, containerHW || 20, circle);
+
+    const ellipseH = verticalR.value * 2;
+    const ellipseW = horizontalR.value * 2;
+    changeParams(ellipseW || 20, ellipseH || 10, ellipse);
+
+    changeParams(squareSide.value || 20, squareSide.value || 20, square);
+
+    changeParams(rectangleSideA.value || 20, rectangleSideB.value || 10, rectangle);
+
+    changeParams(triangleSideA.value || 20, triangleSideB.value || 10, triangle);
   }
 }());
